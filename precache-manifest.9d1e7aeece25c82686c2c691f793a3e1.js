c2495da26626fd3c0268b78d9a60fff8164be7fb
dipsywong98 <dipsywong98@users.noreply.github.com>
2020-10-12 05:21:43 +0000

    deploy: 1478bb0fac3b753db055ef2acf26b258bd199df4

---
+++ precache-manifest.9d1e7aeece25c82686c2c691f793a3e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2262c977b5821a44d1194ad860be33c4",
+    "url": "/Mario-Kart-Tour-Advisor/index.html"
+  },
+  {
+    "revision": "69edf9628e6081a4a9b3",
+    "url": "/Mario-Kart-Tour-Advisor/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a895ecb6231557c45ced",
+    "url": "/Mario-Kart-Tour-Advisor/static/js/2.ce16f7a6.chunk.js"
+  },
+  {
+    "revision": "4a0a7b5d286e7649b2759b31776d7388",
+    "url": "/Mario-Kart-Tour-Advisor/static/js/2.ce16f7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69edf9628e6081a4a9b3",
+    "url": "/Mario-Kart-Tour-Advisor/static/js/main.5a4c7b6e.chunk.js"
+  },
+  {
+    "revision": "46f874f25ffe05763d72",
+    "url": "/Mario-Kart-Tour-Advisor/static/js/runtime-main.10dc452d.js"
+  },
+  {
+    "revision": "e06573ef0f75c68bf29504291170cf8a",
+    "url": "/Mario-Kart-Tour-Advisor/static/media/greenShell.e06573ef.png"
+  },
+  {
+    "revision": "55b594fea34563970443e2db66e6fadd",
+    "url": "/Mario-Kart-Tour-Advisor/static/media/star.55b594fe.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc13ab905c105680d7f06efbd5a260b3.js"
+  "/Mario-Kart-Tour-Advisor/precache-manifest.9d1e7aeece25c82686c2c691f793a3e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mario-Kart-Tour-Advisor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
